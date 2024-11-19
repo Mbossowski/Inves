a8c973bb91aebb3613834830ac644f19cac9e4a9
Mbossowski <92019363+Mbossowski@users.noreply.github.com>
2024-11-19 16:25:23 +0100

    - Created base project

---
+++ src/main/java/com/example/Inves/models/User.java
@@ -0,0 +1,10 @@
+package com.example.Inves.models;
+
+/**
+ * @author Bossowski
+ * @version 1.0
+ * @email Mbossowski01@gmail.com
+ * @date 19/11/2024 - 16:22
+ */
+public class User {
+}
---
+++ src/test/java/com/example/Inves/InvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Inves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
